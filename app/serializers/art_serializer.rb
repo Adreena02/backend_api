476b9ecd269bb86ae68122932070daa5999483d2
Adreena Fulcher <hurlocene@gmail.com>
2021-08-03 17:41:53 -0400

    methods for tags and art work

--- app/serializers/art_serializer.rb
+++ app/serializers/art_serializer.rb
@@ -1,4 +1,5 @@
 class ArtSerializer < ActiveModel::Serializer
   attributes :id, :title, :img_url, :artist_info
-  has_many :likes, :art_tags
+  has_many :likes
+  has_many :art_tags
 end
--- app/serializers/art_tag_serializer.rb
+++ app/serializers/art_tag_serializer.rb
@@ -1,4 +1,5 @@
 class ArtTagSerializer < ActiveModel::Serializer
   attributes :id, :art_id, :tag_id
-  has_many :tags, :arts
+  has_many :tags
+  has_many :arts
 end
