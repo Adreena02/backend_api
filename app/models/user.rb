107bc1024bd8dea56f844f79f833b32feb4bffb3
Terry <incubus8@gmail.com>
2021-08-04 18:08:54 -0400

    started code for suggestion algorithm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
+require 'pry'
 class User < ApplicationRecord
     has_many :likes
     has_many :arts, through: :likes
+    has_many :tags, through: :arts
+
 
     validates :email, presence: true
     validates :name, presence: true
     validates :email, uniqueness: true
     
-
+    def top_tags
+        tags = {}
+        self.tags.each do |tag|
+            if !tags[tag.name]
+                tags[tag.name] = 1
+            else
+                tags[tag.name] +=1
+            end
+        end
+    end
 end
+
+
+# find tags by name, grab paintings
+# tags.max_by(3){|k,v| v}
